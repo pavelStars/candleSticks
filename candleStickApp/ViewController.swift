207bfc739036708316f6ea28e60dfbb0b810614a
Pavel Terziyski <pavel.terziyski@paysafe.com>
2020-05-31 08:15:36 +0300

    Initial Commit

---
+++ candleStickApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  candleStickApp
+//
+//  Created by Pavel Terziyski on 31.05.20.
+//  Copyright © 2020 Pavel Terziyski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
